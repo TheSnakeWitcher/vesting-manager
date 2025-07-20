d0a134bf94e440cffb817547fc62fb80762aa261
Mr.PAPI <thesnakewitcher@gmail.com>
2025-07-20 16:11:36 -0300

    chore: update hardhat.config to use hardhat-gobind

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig, vars } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox-viem"
+import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-deploy"
+import "hardhat-inspect"
+import "@solarity/hardhat-gobind"
 import { polygon, bsc, polygonMumbai, bscTestnet } from "viem/chains"
 
-const PRIVATE_KEY = vars.get("DEV_WALLET_KEY")
+const PRIVATE_KEY = vars.get("DEV_WALLET_KEY");
+const PRIVATE_KEY2 = vars.get("DEV_WALLET_KEY2");
+const ACCOUNT = vars.get("ACCOUNT","0xf830eeF4BB1F6F12eE6442B288f39Cd2772A9Bdc");
 
 const config: HardhatUserConfig = {
     solidity: {
@@ -14,16 +19,32 @@ const config: HardhatUserConfig = {
             },
         }
     },
+    namedAccounts: {
+        deployer: {
+            default: ACCOUNT,
+            hardhat: 0,
+            localhost: 0,
+        },
+        user: {
+            default: "0xE00ad59517d13C2944967a5221FfAEd35419DF90",
+            hardhat: 1,
+            localhost: 1,
+        }
+    },
+    gobind: {
+        outdir: "./data/bindings",
+        onlyFiles: [ "contracts/VestingManager.sol" ],
+    },
     networks: {
         bsc: {
             chainId: bsc.id,
             url: bsc.rpcUrls.default.http[0],
-            accounts: [PRIVATE_KEY!],
+            accounts: [PRIVATE_KEY, PRIVATE_KEY2],
         },
         polygon: {
             chainId: polygon.id,
             url: polygon.rpcUrls.default.http[0],
-            accounts: [PRIVATE_KEY!],
+            accounts: [PRIVATE_KEY, PRIVATE_KEY2],
         },
 
         bscTestnet: {
