13fea043e0e9ca5cfaac7c2253d10432e0cd9f11
Mr.PAPI <thesnakewitcher@gmail.com>
2025-07-20 21:18:37 -0300

    chore: add MockERC20 contract

---
+++ contracts/MockERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(address[] memory beneficiaries) ERC20("VestingFeeToken", "VFT") {
+        for (uint i = 0; i < beneficiaries.length; i++)
+            _mint(beneficiaries[i], 100_000_000_000 * 10 ** decimals());
+    }
+}
